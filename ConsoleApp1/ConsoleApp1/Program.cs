b0df3849b7f6ba3e3e3d201de49de93fd6aad7b8
Julija Gjorgjieva <julija.gjorgjievaa@outlook.com>
2020-01-20 20:54:45 -0800

    Promena f2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Text");
+            Console.WriteLine("Promena f2");
         }
     }
 }
